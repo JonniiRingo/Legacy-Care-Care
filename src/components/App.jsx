7153ec86449ef3166f27c65574933af8d268559b
Jonnii Ringo <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2025-01-18 12:54:42 -0800

    Creation of footer component and local npm install. To do: fix all the file paths first test loaded with no errors but white screen signaling fille paths are not correct

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Navbar"
 import Hero from "./Hero";
 import Features from "./Features";
+import Footer from "./Footer";
 
 
 function App(){
@@ -10,6 +11,7 @@ function App(){
             <Navbar />
             <Hero />
             <Features /> 
+            <Footer />
         </div>
     )
 };
