3fb21751a8358db671f0d071ba974485cb189ec2
Jonnii Ringo <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2025-02-02 13:08:12 -0800

    Staging for integration with routing, sub pages, and chatbot hook

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,8 +9,8 @@ function Hero(){
                     Welcome to Legacy Car Care, where we treat your car as if it was our own. With over 20 years of passion for classic cars and a dedication to excellence, we deliver top-tier car care services that’ll leave your ride shining like new. Whether it’s a Sunday cruiser or your daily workhorse, we’ve got you covered.
                 </p>
                 <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-                    <button type="button" className="btn btn-primary btn-lg px-4 me-sm-3">Primary button</button>
-                    <button type="button" className="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+                    <button type="button" className="btn btn-primary btn-lg px-4 me-sm-3">Create An Account</button>
+                    <button type="button" className="btn btn-outline-secondary btn-lg px-4">Log In </button>
                 </div>
             </div>
             <div className="overflow-hidden" style={{ maxHeight: '30vh' }}>
